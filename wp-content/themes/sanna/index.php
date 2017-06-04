adb7d8ceaccccb18936182a7a644ae77db1317a2
Graziellasanna <dancafg@gmail.com>
2017-06-04 20:13:52 +0200

    Add files via upload

--- wp-content/themes/sanna/index.php
+++ wp-content/themes/sanna/index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div class="row">
+  <div class="row">
     <div class="col-md-12 welcome">
         <h1><?php bloginfo('name') ?></h1>
         <h4><?php bloginfo('description') ?></h4>
@@ -7,32 +7,35 @@
 </div>
 </div>
 </div>
-<div class="row" id="contact">
-    <div class="col-lg-12 text-center">
+<div class="row text-center" id="contact">
         <h2>Contattaci</h2>
         <h3>Ti terremo informato</h3>
-    </div>
 </div>
-<div class="row" id="contact"></div>
-<form class="form-contats">
+<div class="row form-row"></div>
+<form>
   <div class="col-md-6">
+    <div class="form-group">
     <label for="exampleInputEmail1">Indirizzo email </label>
-     <div class="input-group">
-    <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-envelope"></i></span>
-    <input name="email" type="email" class="form-control input-lg" id="exampleInputEmail1" placeholder="Per favore, scrivi  la email" required>
-     </div>
-     </div>
+    <input type="email" class="form-control input-lg" id="exampleInputPassword1" placeholder="Scrivi la tua email" name="email" required>
+        </div>
+     <div class="form-group">
+          <label for="exampleInputPassword1">Email</label>
+          <input type="email" class="form-control input-lg" id="exampleInputPassword1" placeholder="Insert your email" name="email" required>
+        </div>
+
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Per favore, scrivi qui la tua password" required>
   </div>
 </div>
 
-<div class="col-md-6">
-    <label for="exampleInputPassword1">Lascia un commento, grazie.</label>
-    <textarea class="form-control" placeholder="Please, write a message" row="3"></textarea>
+
+    <div class="col-md-6">
+        <label for="exampleInputPassword1">Lascia un commento, grazie</label>
+        <textarea class="form-control input-lg"></textarea>
   </div>   
-    <div class="col-lg-12 text-center">
+    <div class="col-lg-12">
+    <div class="text-center bnt-container" ">
     <button type="submit" class="btn btn-default">Invia</button>
     </div>  
 </div>
