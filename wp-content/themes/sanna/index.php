40fe629ff27f1091452e1cb438552c2fa0bf7013
Graziellasanna <dancafg@gmail.com>
2017-06-04 19:44:03 +0200

    Add files via upload

--- wp-content/themes/sanna/index.php
+++ wp-content/themes/sanna/index.php
@@ -9,15 +9,18 @@
 </div>
 <div class="row" id="contact">
     <div class="col-lg-12 text-center">
-        <h1>Contattaci</h1>
+        <h2>Contattaci</h2>
         <h3>Ti terremo informato</h3>
     </div>
 </div>
+<div class="row" id="contact"></div>
 <form class="form-contats">
- <div class="col-md-6">
-     <div class="form-group">
+  <div class="col-md-6">
     <label for="exampleInputEmail1">Indirizzo email </label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Per favore, scrivi qui la tua email" required>
+     <div class="input-group">
+    <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-envelope"></i></span>
+    <input name="email" type="email" class="form-control input-lg" id="exampleInputEmail1" placeholder="Per favore, scrivi  la email" required>
+     </div>
      </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
@@ -26,8 +29,8 @@
 </div>
 
 <div class="col-md-6">
-    <label for="exampleInputPassword1">Lascia un messaggio</label>
-    <textarea class="form-control" placeholder="Lascia un messaggio" row="3"></textarea>
+    <label for="exampleInputPassword1">Lascia un commento, grazie.</label>
+    <textarea class="form-control" placeholder="Please, write a message" row="3"></textarea>
   </div>   
     <div class="col-lg-12 text-center">
     <button type="submit" class="btn btn-default">Invia</button>
